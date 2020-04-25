0c2e0e17f5ab861db697ae165cf4331d135b3e47
Valerie Foster <fosterv2@outlook.com>
2020-04-25 00:15:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const multiply = function() {
   return num1 * num2
 }
 
-const random = function() {
-  return Math.ciel(Math.random() * 100)
-}
+const random =  Math.ciel(Math.random() * 100)
+
 
 const mod = function(num1, num2) {
 
