3201e9df31c750068db86fc5fd7c9fb2dbc31a69
Valerie Foster <fosterv2@outlook.com>
2020-04-25 00:16:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code here
 
 const multiply = function() {
-  let num1 = 31
-  let num2 = 2
+  var num1 = 31
+  var num2 = 2
   return num1 * num2
 }
 
-const random =  Math.ciel(Math.random() * 100)
+var random =  Math.ciel(Math.random() * 100)
 
 
 const mod = function(num1, num2) {
