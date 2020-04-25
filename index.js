6ead70fe6f39fb7adc33777cef15ff1cd94f3dcf
Valerie Foster <fosterv2@outlook.com>
2020-04-25 00:09:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Write your code here
 
 const multiply = function(num1, num2) {
-  
+
 }
 
 const random = function(num1, num2) {
-  
+
 }
 
 const mod = function(num1, num2) {
-  
+
 }
 
 const max = function(num1, num2) {
-  
+
 }
