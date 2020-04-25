5aa70a707be4586a55863cca2ee92ac91dd0283f
Valerie Foster <fosterv2@outlook.com>
2020-04-25 00:12:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your code here
 
-const multiply = function(num1, num2) {
+const multiply = function(num1 = 31, num2 = 2) {
   return num1 * num2
 }
 
-const random = function(num1, num2) {
-
+const random = function() {
+  return Math.ciel(Math.random() * 100)
 }
 
 const mod = function(num1, num2) {
